a9c7f739982044f426a2aecac42f00e26a6c4bd9
Danielle Modelli <danielle_modelli@hotmail.com>
2024-02-29 23:11:10 -0300

    update branch

--- src/pages/Info/index.tsx
+++ src/pages/Info/index.tsx
@@ -1,9 +1,35 @@
-// import styles from './Info.module.scss';
+import styles from './Info.module.scss';
+import stylesTheme from 'styles/theme.module.scss';
+import casa from 'assets/info/casa.png';
+import massa1 from 'assets/info/massa1.png';
+import massa2 from 'assets/info/massa2.png';
 
+const imagens = [massa1, massa2];
 export default function Info() {
   return (
     <section>
-      Sobre
+      <h3 className={stylesTheme.title}>Sobre</h3>
+      <div className={styles.sobreNos}>
+        <img src={casa} alt="Casa Aluroni" />
+        <div className={styles.sobreNos__texto}>
+          <p>
+            Nós do Aluroni oferecemos a vocês, nossos queridos clientes, a Massa Italiana Caseira mais saborosa e sofisticada de São Paulo! Prezamos pelos ingredientes tradicionais da culinária Italiana, frescos e de excelente qualidade para que sua experiência seja ainda mais intensa!
+          </p>
+          <p>
+            Também possuímos uma cardápio de carnes com muitas opções de acordo com o seu gosto!
+          </p>
+          <p>
+            Para acompanhar as massas italianas, Aluroni possui uma reserva de vinhos especiais, que harmonizam perfeitamente com o seu parto, seja carne ou massa!
+          </p>
+        </div>
+      </div>
+      <div className={styles.imagens}>
+        {imagens.map((imagem, index) => (
+          <div key={index} className={styles.imagens__imagem}>
+            <img src={imagem} alt="imagem de massa" />
+          </div>
+        ))}
+      </div>
     </section>
   );
 }
